8dbc40b5be1bd10af926a93e808a621802c06308
Yaswanth Varma <ymudunur@gitam.in>
2025-09-27 12:33:50 +0530

    Updated commit with correct author info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Yaswanth Varma',
+    author_email= 'ymudunur@gitam.in',
+    packages= find_packages(),
+    install_requires= []
+)
